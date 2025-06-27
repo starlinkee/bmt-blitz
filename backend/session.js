135b3efd15c58392eebf4a2ee7d696472a3983e4
Viktor <contact.starlinkee@gmail.com>
2025-06-28 00:29:52 +0200

    quick commit

--- backend/session.js
+++ backend/session.js
@@ -1,6 +1,6 @@
 import session from 'express-session';
 import dotenv from 'dotenv';
-dotenv.config({ path: './backend/.env' });     // ważne: absolutny path
+dotenv.config({ path: './backend/.env' });     // ważne: absolutny pathd
 
 export const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET || 'change-me',
