4a4c95325618a062d5b176f7ced91924e7f45977
Viktor <contact.starlinkee@gmail.com>
2025-07-01 21:39:28 +0200

    quick commit

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Ustawiamy katalog bieżącego pliku (główny katalog)
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Ładujemy .env z katalogu backend
+dotenv.config({ path: path.join(__dirname, 'backend/.env') });
+
 import app from './backend/src/server.js';
 
 export default app;
